0b0a7a64d4cb03a1d7e523294c3cf49c261a5637
Felix Rochau <felixrochau@gmx.de>
2022-04-02 13:13:40 +0200

    add documentation

--- src/predict.py
+++ src/predict.py
@@ -1,40 +1,60 @@
-import sys
+from logging import getLogger
 import pandas as pd
-import pickle
-from sklearn.metrics import mean_squared_error
 import warnings
-import mlflow
+import os
 from mlflow.sklearn import load_model
 
+from src.hdd_preprocessing import load_preprocess_testdata
+from src.hdd_feature_engineering import hdd_preprocessor
+
 warnings.filterwarnings("ignore")
+logger = getLogger(__name__)
 
-from feature_engineering import (
-    fill_missing_values,
-    drop_column,
-    transform_altitude,
-)
+def __get_data():
+    """Load and preprocess sample test dataset.
 
-print("Number of arguments:", len(sys.argv), "arguments.")
-print("Argument List:", str(sys.argv))
+    Returns:
+        _type_: X_test
+    """
+    X_test = load_preprocess_testdata(days=30,
+                filename="ST4000DM000_history_total", 
+                path=os.getcwd())
+    return X_test
 
-# in an ideal world this would validated
-model_path = sys.argv[1]
-X_test_path = sys.argv[2]
-y_test_path = sys.argv[3]
+def __get_model():
+    """Load the saved model for prediction.
 
-# load the model from disk
-# model_path = "models/linear"
-loaded_model = load_model(model_path)
-X_test = pd.read_csv(X_test_path)
-y_test = pd.read_csv(y_test_path)
+    Returns:
+        _type_: Model
+    """
+    model_path = "models/deployment_stacked"
+    model = load_model(model_path)
+    return model
 
-# feature eng on test data
-print("Feature engineering")
-X_test = transform_altitude(X_test)
-X_test = drop_column(X_test, col_name="Unnamed: 0.1")
-X_test = drop_column(X_test, col_name="Quakers")
-X_test = fill_missing_values(X_test)
+def run_predict():
+    """Load the test data, preprocess it and run the prediction.
 
-y_test_pred = loaded_model.predict(X_test)
-mse_test = mean_squared_error(y_test, y_test_pred)
-print(f"MSE on test is: {mse_test}")
+    Returns:
+        _type_: Predicted targets
+    """
+    logger.info("Loading model")
+    model = __get_model()
+    logger.info("Loading and preprocessing data")
+    X_test = __get_data()
+    logger.info("Feature engineering on test")
+    preprocessor = hdd_preprocessor(days=30, trigger=0.05)
+    X_test = preprocessor.fit_transform(X_test) # Nothing saved in the fit!
+    logger.info("Prediction in progress")
+    y_proba = model.predict_proba(X_test)
+    return y_proba > 0.15
+
+if __name__ == "__main__":
+    import logging
+
+    logger = logging.getLogger()
+    logging.basicConfig(format="%(asctime)s: %(message)s")
+    logging.getLogger("pyhive").setLevel(logging.CRITICAL)  # avoid excessive logs
+    logger.setLevel(logging.INFO)
+
+    y_pred = run_predict()
+    print(y_pred)
