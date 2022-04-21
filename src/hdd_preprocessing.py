2e210c6f9e2c83850feef1c0ac834335e2a0ddf3
Felix Rochau <felix@MBP-Felix.fritz.box>
2022-04-21 12:55:28 +0200

    testing with flake8

--- src/hdd_preprocessing.py
+++ src/hdd_preprocessing.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import os
 
-def load_drive_stats(filename:str, path:str) -> pd.DataFrame:
+
+def load_drive_stats(filename: str, path: str) -> pd.DataFrame:
     """Load drive stats file
 
     Args:
@@ -15,6 +16,7 @@ def load_drive_stats(filename:str, path:str) -> pd.DataFrame:
     df = pd.read_csv(file, parse_dates=["date"])
     return df
 
+
 def calculate_target(df_in, days=30):
     """Merge failure date, calculate the countdown and the target.
 
@@ -25,20 +27,25 @@ def calculate_target(df_in, days=30):
     Returns:
         pd.Series: Target variable
     """
-    df = df_in.copy() # Copy to protect input dataframe
+    df = df_in.copy()  # Copy to protect input dataframe
     # Series of all the hdds the day they failed to obtain failure date
     failure = df[df.failure == 1]
     # Only use first failure per hdd
     failure = failure.sort_values('date')
     failure = failure.drop_duplicates(keep='first', subset="serial_number")
     # Assign failure dates
-    date_failure = df['serial_number'].map(failure.set_index('serial_number')['date'])
+    date_failure = (df['serial_number']
+                    .map(failure.set_index('serial_number')
+                    ['date'])
+                    )
     # Days to fail as int
     countdown = (date_failure - df.date).dt.days
-    # Remove observations with negative countdown (repaired drives) and with more than 800 days left
+    # Remove observations with negative countdown (repaired drives) and with
+    # more than 800 days left
     df = df[(countdown >= 0) & (countdown < 800)]
     target = (countdown <= days)[(countdown >= 0) & (countdown < 800)]
     return df, target
+
 
 def train_test_splitter(X, y, test_size=0.3, random_state=42) -> pd.DataFrame:
     """Train test split of the drive data
@@ -47,7 +54,8 @@ def train_test_splitter(X, y, test_size=0.3, random_state=42) -> pd.DataFrame:
         X (_type_): Feature variable
         y (_type_): Target variable
         test_size (float, optional): Size of the test subset. Defaults to 0.3.
-        random_state (int, optional): Random state for comparability over different runs. Defaults to 42.
+        random_state (int, optional): Random state for comparability over
+        different runs. Defaults to 42.
 
     Returns:
         pd.DataFrame: _description_
@@ -55,7 +63,9 @@ def train_test_splitter(X, y, test_size=0.3, random_state=42) -> pd.DataFrame:
     # All the unique serial numbers
     drives = pd.Series(X.serial_number.unique(), name="HDD")
     # Random sampling of drives
-    drives_test = drives.sample(int(test_size * len(drives)), random_state=random_state)
+    drives_test = drives.sample(int(test_size * len(drives)),
+                                random_state=random_state
+                                )
     # Remaining drives end up in the train set
     drives_train = drives.drop(drives_test.index, axis=0)
     # Create split X and y
@@ -65,8 +75,10 @@ def train_test_splitter(X, y, test_size=0.3, random_state=42) -> pd.DataFrame:
     y_test = y[X.serial_number.isin(drives_test)]
     return X_train, X_test, y_train, y_test
 
+
 def drop_cols(df_in) -> pd.DataFrame:
-    """Drop columns with missing values. A threshold allows to tune which columns are dropped.
+    """Drop columns with missing values. A threshold allows to tune
+    which columns are dropped.
 
     Args:
         df (_type_): Drive stats data
@@ -74,14 +86,17 @@ def drop_cols(df_in) -> pd.DataFrame:
     Returns:
         pd.DataFrame: Drive stats file with dropped columns
     """
-    cols_of_importance = ['smart_4_raw', 'smart_5_raw', 'smart_7_raw', 'smart_9_raw',
-                            'smart_12_raw', 'smart_183_raw', 'smart_184_raw', 'smart_187_raw',
-                            'smart_188_raw', 'smart_189_raw', 'smart_190_raw', 'smart_192_raw',
-                            'smart_193_raw', 'smart_194_raw', 'smart_197_raw', 'smart_198_raw',
-                            'smart_199_raw', 'smart_240_raw', 'smart_241_raw', 'smart_242_raw',
-                            'serial_number', 'date']
-    df = df_in.loc[:,cols_of_importance]
+    cols_of_importance = ['smart_4_raw', 'smart_5_raw', 'smart_7_raw',
+                          'smart_9_raw', 'smart_12_raw', 'smart_183_raw',
+                          'smart_184_raw', 'smart_187_raw', 'smart_188_raw',
+                          'smart_189_raw', 'smart_190_raw', 'smart_192_raw',
+                          'smart_193_raw', 'smart_194_raw', 'smart_197_raw',
+                          'smart_198_raw', 'smart_199_raw', 'smart_240_raw',
+                          'smart_241_raw', 'smart_242_raw', 'serial_number',
+                          'date']
+    df = df_in.loc[:, cols_of_importance]
     return df
+
 
 def drop_missing_rows(df_in) -> pd.DataFrame:
     """Drop rows with missing values (measurement errors, see EDA)
@@ -95,6 +110,7 @@ def drop_missing_rows(df_in) -> pd.DataFrame:
     df = df_in.copy().dropna(how="any")
     return df
 
+
 def drop_duplicate_rows(df_in) -> pd.DataFrame:
     """Drop doublicated rows (measurement errors, see EDA)
 
@@ -104,8 +120,11 @@ def drop_duplicate_rows(df_in) -> pd.DataFrame:
     Returns:
         pd.DataFrame: Drive stats data with removed rows
     """
-    df = df_in.copy().drop_duplicates(keep='first', subset=["serial_number", "date"])
+    df = (df_in.copy()
+          .drop_duplicates(keep='first', subset=["serial_number", "date"])
+          )
     return df
+
 
 def remove_smart_7_outliers(df_in, threshold=5e10) -> pd.DataFrame:
     """_summary_
@@ -123,62 +142,78 @@ def remove_smart_7_outliers(df_in, threshold=5e10) -> pd.DataFrame:
         df = df.drop(df[df.serial_number == sn].index)
     return df
 
-def load_preprocess_data(filename="ST4000DM000_history_total", path=os.getcwd(), days=30) -> pd.DataFrame:
+
+def load_preprocess_data(filename="ST4000DM000_history_total",
+                         path=os.getcwd(),
+                         days=30
+                         ) -> pd.DataFrame:
     """Load and preprocess drive stats data
 
     Args:
-        filename (str, optional): Name of the csv file. Defaults to "ST4000DM000_history".
+        filename (str, optional): Name of the csv file. Defaults to
+            "ST4000DM000_history".
         path (_type_, optional): Path of the repo. Defaults to os.getcwd().
 
     Returns:
         pd.DataFrame: Dataframe with the drive stats data
     """
-    #print("Preprocessing")
-    #print("Loading file", filename)
+    # print("Preprocessing")
+    # print("Loading file", filename)
     X = load_drive_stats(filename, path)
-    #print("Calculate the target variable")
+    # print("Calculate the target variable")
     X, y = calculate_target(X)
-    #print("Removing smart_7_raw outliers")
+    # print("Removing smart_7_raw outliers")
     X = remove_smart_7_outliers(X)
-    #print("Dropping unused columns")
+    # print("Dropping unused columns")
     X = drop_cols(X)
-    #print("Dropping missings")
+    # print("Dropping missings")
     X = drop_missing_rows(X)
-    #print("Dropping dublicated observations")
+    # print("Dropping dublicated observations")
     X = drop_duplicate_rows(X)
     y = y[X.index]
-    #print("Preprocessing finished")
-    #print("-----------------------------------------------------")
+    # print("Preprocessing finished")
+    # print("-----------------------------------------------------")
     return X, y
 
-def load_preprocess_testdata(filename="ST4000DM000_history_total", path=os.getcwd(), days=30) -> pd.DataFrame:
-    """Load and preprocess drive stats data that does not contain information about the target.
+
+def load_preprocess_testdata(filename="ST4000DM000_history_total",
+                             path=os.getcwd(),
+                             days=30
+                             ) -> pd.DataFrame:
+    """Load and preprocess drive stats data that does not contain information
+    about the target.
 
     Args:
-        filename (str, optional): Name of the csv file. Defaults to "ST4000DM000_history".
+        filename (str, optional): Name of the csv file. Defaults to
+            "ST4000DM000_history".
         path (_type_, optional): Path of the repo. Defaults to os.getcwd().
 
     Returns:
         pd.DataFrame: Dataframe with the drive stats data
     """
-    #print("Preprocessing")
-    #print("Loading file", filename)
+    # print("Preprocessing")
+    # print("Loading file", filename)
     df = load_drive_stats(filename, path)
-    #print("Dropping unused columns")
+    # print("Dropping unused columns")
     df = drop_cols(df)
-    #print("Dropping missings")
+    # print("Dropping missings")
     df = drop_missing_rows(df)
-    #print("Dropping dublicated observations")
+    # print("Dropping dublicated observations")
     df = drop_duplicate_rows(df)
-    #print("Preprocessing finished")
-    #print("-----------------------------------------------------")
+    # print("Preprocessing finished")
+    # print("-----------------------------------------------------")
     return df
 
-def save_preprocessed_data(filename="ST4000DM000_history_total", path=os.getcwd()):
-    """Load and preprocess the drive stats data and store the result in a csv file
+
+def save_preprocessed_data(filename="ST4000DM000_history_total",
+                           path=os.getcwd()
+                           ):
+    """Load and preprocess the drive stats data and store the result
+    in a csv file.
 
     Args:
-        filename (str, optional): Name of the csv file. Defaults to "ST4000DM000_history".
+        filename (str, optional): Name of the csv file. Defaults to
+            "ST4000DM000_history".
         path (_type_, optional): Path of the repo. Defaults to os.getcwd().
 
     Returns:
@@ -192,5 +227,9 @@ def save_preprocessed_data(filename="ST4000DM000_history_total", path=os.getcwd(
     df.to_csv(file, index=False)
     return df
 
+
 if __name__ == "__main__":
-    df, y = load_preprocess_data(days=30, filename="ST4000DM000_history_total", path=os.getcwd())
+    df, y = load_preprocess_data(days=30,
+                                 filename="ST4000DM000_history_total",
+                                 path=os.getcwd()
+                                 )
