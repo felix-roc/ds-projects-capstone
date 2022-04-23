d7046bfaa24806b64f0d71807519bf3afda678ab
Felix Rochau <felix@MBP-Felix.fritz.box>
2022-04-23 20:38:44 +0200

    style: cleanup of script

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,6 @@ warnings.filterwarnings('ignore')
 container_intro = st.container()
 container_test_data = st.container()
 container_upload_data = st.container()
-expander_upload_data = st.expander(label='Click to expand')
 
 # URL for request to backend
 url = 'https://hdd-predicitve-maintenance-api.herokuapp.com/'
@@ -32,23 +31,6 @@ def predict_rating(df, url=url):
     r = requests.post(url, json=data, headers=headers)
     y_pred = r.text
     return y_pred
-
-
-# # function for loading some data from the test data
-# @st.cache
-# def load_data(file):
-#     """Load the stored test data and sample something.
-#     Not sure what this function does though and not using it?
-
-#     Args:
-#         file (_type_): filename
-
-#     Returns:
-#         _type_: _description_
-#     """
-#     data = pd.read_csv(file)
-#     model_pred = data.sample(30)
-#     return model_pred
 
 
 # Sidebar
@@ -84,58 +66,65 @@ with container_intro:
         "This is a web app to predict if a HDD drive will fail or not fail in"
         " the next 30 days.")
     st.write(
-        "Please click on the Predict button to see the results of the"
+        "Please click on the Predict button to see the results of the "
         "classification.")
-    # st.header('')
 
 
 # Test Data Container
 with container_test_data:
     st.markdown('**This is how a random sample of our raw data looks like:**')
+    # Read the provided test data
     data = pd.read_csv('file/test_data_final.csv')
+    # Sample a random drive from the list
     serial = data.sample(1)['serial_number'].to_list()[0]
     data = data[data['serial_number'] == serial]
+    # Sort values chronologically
     data.sort_values('date', inplace=True, ascending=False)
+    # Display the first 5 entries
     st.write(data.head(5))
 
-    # predicting on the prepared test data df
+    # predicting on the prepared test data
     if st.button('Predict on our provided test data'):
         y_pred = predict_rating(url, data)
         y_pred = y_pred.split(':')[1]
         y_pred = y_pred.split(',')[0]
         if y_pred == "false":
-            st.write('__**Your hard drive will not fail in the next 30 days!**__')
+            st.write(
+                '__**The HDD will NOT fail in the next 30 days**__')
             st.balloons()
         else:
-            st.write('__**Your hard drive might fail in the next 30 days!**__')
-
-    st.header('')
-    st.header('')
+            st.write('__**The HDD will fail in the next 30 days!**__')
 
 
-# UPLOAD CONTAINER
-# subheader for uploading own data
+# Upload Container
 with container_upload_data:
     st.image('jpg/HDD.jpg')
     st.subheader('Want to predict for your own hard drive?')
-    # with expander_upload_data:
-    #upload a file
+    # Upload a file
     dataframe_upload = None
-    uploaded_file = st.file_uploader("Choose your file for your own hard drive to upload. Make sure it's only data for the model 'ST4000DM000' from Seagate.", help= 'Drag your files here')
+    uploaded_file = st.file_uploader(
+        "Choose your file for your own hard drive to upload. Make sure it's "
+        "only data for the model 'ST4000DM000' from Seagate.",
+        help='Drag your files here')
     if uploaded_file is not None:
+        # Read to uploaded file
         dataframe_upload = pd.read_csv(uploaded_file)
+        # Sort rows chronologically
         dataframe_upload.sort_values('date', inplace=True, ascending=False)
+        # Display header
         st.write(dataframe_upload.head(5))
-        st.success('Your file was successfully uploaded!')
+        st.success('Your file was uploaded successfully!')
 
-        # Predicting HDD failure on your own data
-        # executing preprocessing via pipeline
         if st.button('Predict'):
+            # Predict on own data using API
             y_pred = predict_rating(url, dataframe_upload)
+            # Process the returned predictions
             y_pred = y_pred.split(':')[1]
             y_pred = y_pred.split(',')[0]
             if y_pred == "false":
-                st.write('__**Your hard drive will not fail in the next 30 days!**__')
+                st.write(
+                    '__**Your hHDD will NOT fail in the next 30 days!**__')
                 st.balloons()
             else:
-                st.write('__**Your hard drive might fail in the next 30 days!**__')
+                st.write(
+                    '__**Your HDD will fail in the next 30 days!**__')
