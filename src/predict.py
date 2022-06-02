79707b4e05b38a6bddd9b4394556f759720b1404
felix-roc <felixrochau@gmx.de>
2022-06-02 09:57:06 +0200

    minor fixes

--- src/predict.py
+++ src/predict.py
@@ -17,9 +17,8 @@ def __get_data():
         _type_: X_test
     """
     X_test = load_preprocess_testdata(
-        days=30,
-        filename="ST4000DM000_history_total",
-        path=os.getcwd())
+        days=30, filename="ST4000DM000_history_total", path=os.getcwd()
+    )
     return X_test
 
 
@@ -29,7 +28,7 @@ def __get_model():
     Returns:
         _type_: Model
     """
-    model_path = "models/deployment_stacked"
+    model_path = "models/deployment_ann"
     model = load_model(model_path)
     return model
 
@@ -46,14 +45,15 @@ def run_predict():
     X_test = __get_data()
     logger.info("Feature engineering on test")
     preprocessor = hdd_preprocessor(days=30, trigger=0.05)
-    X_test = preprocessor.fit_transform(X_test)  # Nothing saved in the fit!
+    X_test = preprocessor.fit_transform(X_test)  # Nothing saved in the fit
     logger.info("Prediction in progress")
     y_proba = model.predict_proba(X_test)
-    return y_proba > 0.15
+    return y_proba > 0.5
 
 
 if __name__ == "__main__":
     import logging
+    import pandas as pd
 
     logger = logging.getLogger()
     logging.basicConfig(format="%(asctime)s: %(message)s")
@@ -63,3 +63,4 @@ if __name__ == "__main__":
 
     y_pred = run_predict()
     print(y_pred)
+    pd.Series(y_pred[:, 0]).to_csv("data/processed/y_pred.csv", index=False)
